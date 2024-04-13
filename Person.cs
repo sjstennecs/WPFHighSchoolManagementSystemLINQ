6b040110c99e6ee5c5028b10a75e94ec6610723b
Steven Stenner <Stevenstennercs@gmail.com>
2024-04-13 17:18:18 +0900

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace WPFHighSchoolManagementSystemLINQ
+{
+    internal class Person
+    {
+        private string _name;
+        private int _age;
+        private string _gender;
+
+        public string Name { get { return _name; } set { _name = value; } }
+        public int Age { get { return _age; } set { _age = value; } }
+        public string Gender { get { return _gender; } set { _name = value; } }
+
+
+        public Person() {
+            // Default constructor for person object
+            Name = "John Doe";
+            Age = 18;
+            Gender = "Male";
+        }
+
+        // Overloaded constructor
+        public Person(string name, int age, string gender)
+        {
+            Name = name;
+            Age = age;
+            Gender = gender;
+        }
+
+        public string PersonToString()
+        {
+            return (Name + ", age " + Age + ", " + Gender);
+        }
+    }
+}
