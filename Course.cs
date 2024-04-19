286523dd12173bb6dde061abbb97d05f20d52f16
Steven Stenner <Stevenstennercs@gmail.com>
2024-04-19 15:41:30 +0900

    Added and implemented Teacher.cs and Course.cs classes.

---
+++ Course.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFHighSchoolManagementSystemLINQ
+{
+    internal class Course
+    {
+        // properties
+        private int _id;
+        private string _name;
+        private string _description;
+        private List<Course> _prerequisites;
+        private List<Student> _students;
+        private Teacher _teacher;
+
+        // getters and setters (we will add logic for the list of students and the list of prerequisites in the methods section)
+
+        public int CourseId { get { return _id; } set { _id = value; } }
+        public string Name { get { return _name;} set { _name = value; } }
+        public string Description { get { return _description;} set { _description = value; } }
+        public Teacher Teacher { get { return _teacher; } set { _teacher = value; } }
+
+        // constructor
+        public Course(int id, string name, string description, List<Course> prereqs, List<Student> students, Teacher teacher)
+        {
+            _id = id;
+            _name = name;
+            _description = description;
+            _prerequisites = prereqs;
+            _students = students;
+            _teacher = teacher;
+        }
+        
+        // methods
+        // Start with methods concerning the Prereqs list
+        public string PrintPrerequisites()
+        {
+            string outputString = $"Prerequisites for {this.Name}:";
+            foreach (Course course in _prerequisites)
+            {
+                outputString += $"{course.Name} ";
+            }
+            return outputString;
+        }
+
+        public void AddPrerequisite(Course course)
+        {
+            this._prerequisites.Add(course);
+        }
+
+        public void RemovePrerequisiteByName(string targetName)
+        {
+            // perfect place for using LINQ
+        }
+
+        public void RemovePrerequisiteByID(int targetID)
+        {
+            // perfect place for using LINQ
+        }
+    }
+}
--- Teacher.cs
+++ Teacher.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace WPFHighSchoolManagementSystemLINQ
 {
-    internal class Teacher
+    internal class Teacher : Person
     {
+        // properties
+        private int _salary;
+        private DateTime _csd; // contract start date
+        private DateTime _ced; // contract end date
+
+        public int Salary {  get { return _salary; } set {  _salary = value; } }
+        public DateTime StartDate { get { return _csd; } set { _csd = value; } }
+        public DateTime EndDate { get { return _ced; } set { _ced = value; } }
+
+        // constructors
+        public Teacher(string name, int age, string gender, int sal, string startdate, string enddate):base(name,age,gender)
+        {
+
+        }
+
+        // methods
+        public int YearsLeftAtSchool()
+        {
+            return EndDate.Year - StartDate.Year;
+        }
     }
 }
